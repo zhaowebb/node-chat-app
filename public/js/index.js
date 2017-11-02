0e319ca92d22f1762325f0cde95ed9ee4c1c1877
zhaowebb <zhaowebb@gmail.com>
2017-11-02 15:38:02 -0400

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('connected to server')
 
-	socket.emit('createMessage', {
-		from: 'andrew',
-		text: 'how are  you'
-	})
 });
 
 socket.on('disconnect', function(){
