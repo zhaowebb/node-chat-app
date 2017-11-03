a862260a7bac4ea4d6f22927371fca3c35d775a4
zhaowebb <zhaowebb@gmail.com>
2017-11-03 19:45:11 -0400

    add users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -3,22 +3,40 @@ const http = require('http');
 const express = require('express');
 const port = process.env.PORT || 3333;
 const publicPath = path.join(__dirname, '../public');
-const socketIO = require('socket.io')
+const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message')
+const {isRealString} = require('./utils/validation.js');
+const {Users} = require('./utils/users');
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('new user connected');
 
-	socket.emit("newMessage", generateMessage(
-		'Admin', 'welcom to the chat app'));
+	
 
-	socket.broadcast.emit('newMessage', generateMessage(
-		'Admin', 'new user joined'));
+	socket.on('join', (params, callback) => {
+		if(!isRealString(params.name) || !isRealString(params.room)){
+			return callback('Name and room name are required');
+		}
+
+		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+		
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		socket.emit("newMessage", generateMessage(
+			'Admin', 'welcom to the chat app'));
+
+		socket.broadcast.to(params.room).emit('newMessage', generateMessage(
+			'Admin', `${params.name} has joined`));
+		callback();
+	});
 
 	socket.on('createMessage', (message, callback) => {
 		console.log('createMessage ', message);
@@ -43,7 +61,11 @@ io.on('connection', (socket) => {
 
 
 	socket.on('disconnect', () => {
-		console.log('user was disconnected');
+		var user = users.removeUser(socket.id);
+		if(user){
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+		}
 	});
 });
 
